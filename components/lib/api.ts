a614b15d227d2e6138bf44a1a4c116a3af6b13bb
MariaApple17 <mariaappleroncales@gmail.com>
2026-03-09 16:20:34 +0800

    updated

--- components/lib/api.ts
+++ components/lib/api.ts
@@ -1,30 +1,22 @@
-import axios from 'axios';
-
-const baseURL =
-  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001/api';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL,
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001/api",
   withCredentials: true,
 });
 
-/* =========================
-   REQUEST INTERCEPTOR
-========================= */
-api.interceptors.request.use(
-  (config) => {
-    if (typeof window !== 'undefined') {
-      const token = sessionStorage.getItem('token');
+api.interceptors.request.use((config) => {
+  if (typeof window !== "undefined") {
+    const token = sessionStorage.getItem("token");
 
-      if (token) {
-        config.headers = config.headers || {};
-        config.headers.Authorization = `Bearer ${token}`;
-      }
+    if (token) {
+      config.headers = config.headers || {};
+      config.headers.Authorization = `Bearer ${token}`;
     }
+  }
 
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+  return config;
+});
 
 export default api;
